da7be98b91c0d1fad323ce5e1c7f9318aca24101
nao_lahera <lahera0103@gmail.com>
2024-03-27 14:29:51 -0400

    Completando la jeraquia del AST

--- semantic_checking/AST.py
+++ semantic_checking/AST.py
@@ -32,39 +32,24 @@ class BinaryNode(Node):
         raise NotImplementedError()
     
 #-------------------------------------------------------------------------------------------------------------------------------------------------#
-
 class ProgramNode(Node):
     def __init__(self, statments) -> None:
         super().__init__()
         self.statments = statments
-        
-# class StatmentNode(Node):
-#     def __init__(self, node) -> None:
-#         super().__init__()
-#         self.node = node
-        
-# class NonCreateStatmentNode(StatmentNode):
-#     def __init__(self, node) -> None:
-#         super().__init__()
-#         self.node = node
-        
-# class CreateStatmentNode(StatmentNode):
-#     def __init__(self, node) -> None:
-#         super().__init__()
-#         self.node = node
-        
+             
 class PrintStatmentNode(Node):
     def __init__(self, expression) -> None:
         super().__init__()
         self.expression = expression
         
+#TODO Se debe poner el type definition en el asigment qu ecreo que en la gramatica no esta hecho y luego ponerlo aqui tambien
 class KernAssigmentNode(Node):
     def __init__(self, id, expression) -> None:
         super().__init__()
         self.id = id
         self.expression = expression
         
-# TODO Podriamos instancias la clase Type
+# TODO Podriamos instanciar la clase Type
 class TypeNode(Node):
     def __init__(self, type) -> None:
         super().__init__()
@@ -77,6 +62,8 @@ class FunctionDefinitionNode(Node):
         self.type_annotation = type_annotation
         self.parameters = parameters
         self.body = body
+        
+#--------------------------------Non_Create-Statment-----------------------------------------------------------------------------------------------------------------------#
         
 class IfStructureNode(Node):
     def __init__(self, condition, body, _elif, _else) -> None:
@@ -111,6 +98,45 @@ class ForStructureNode(Node):
         self.increment_condition = increment_assigment
         self.body = body
         
+#-----------------------------------Class----------------------------------------------------------------------------------------------#
+class TypeDefinitionNode(Node):
+    def __init__(self, id, inheritance, attributes, methods) -> None:
+        super().__init__()
+        self.id = id
+        self.inheritance = inheritance
+        self.attribute = attributes
+        self.methods = methods
+        
+#TODO Esto debe recibir un type annotation?
+class MethodsDefinitionNode(Node):
+    def __init__(self, id, parameters, body) -> None:
+        super().__init__()
+        self.id = id
+        self.parameters = parameters
+        self.body = body
+        
+class Inheritance(Node):
+    def __init__(self, type) -> None:
+        super().__init__()
+        self.type = type
+        
+class InstanceCreationNode(Node):
+    def __init__(self, id, type, arguments) -> None:
+        super().__init__()
+        self.id = id
+        self.type = type
+        self.arguments = arguments
+        
+#TODO Ver bien que en que consiste el member acces
+class MemberAccesNode(Node):
+    def __init__(self, base_object, object_property_to_acces, args) -> None:
+        super().__init__()
+        self.base_object = base_object
+        self.object_property_ti_acces = object_property_to_acces
+        self.args = args
+        
+#! No son necesarios los operadores
+#------------------------------------Operators----------------------------------------------------------------------------------------------------#       
 class BooleanOperator(Node):
     def __init__(self, operator) -> None:
         super().__init__()
@@ -121,17 +147,98 @@ class AritmeticOperator(Node):
         super().__init__()
         self.operator = operator
         
-class BooleanExpression(Node):
-    def __init__(self, expression) -> None:
+class ConcatOperator(Node):
+    def __init__(self, operator) -> None:
         super().__init__()
-        self.expression = expression
+        self.operator = operator
+        
+#-------------------------------------------Abstrct-Expressions------------------------------------------------------------------------------------------#
+class BooleanExpression(Node):
+    def __init__(self, expression_1, expression_2) -> None:
+        super().__init__()
+        self.expression = expression_1
+        self.expressiin_2 = expression_2
         
 class AritmeticExpression(Node):
-    def __init__(self, expression) -> None:
+    def __init__(self, expression_1, expression_2) -> None:
+        super().__init__()
+        self.expression_1 = expression_1
+        self.expression_2 = expression_2
+        
+#-------------------------------Aritmetic-Expressions-------------------------------------------------------------------------------------------------#
+class PlusExpressionNode(AritmeticExpression):
+    def __init__(self, expression_1, expresion_2) -> None:
+        super().__init__(expression_1, expresion_2)
+        
+class SubsExpressionNode(AritmeticExpression):
+    def __init__(self, expression_1, expresion_2) -> None:
+        super().__init__(expression_1, expresion_2)
+        
+class DivExpressionNode(AritmeticExpression):
+    def __init__(self, expression_1, expresion_2) -> None:
+        super().__init__(expression_1, expresion_2)
+        
+class MultExpressionNode(AritmeticExpression):
+    def __init__(self, expression_1, expresion_2) -> None:
+        super().__init__(expression_1, expresion_2)
+        
+class ModExpressionNode(AritmeticExpression):
+    def __init__(self, expression_1, expresion_2) -> None:
+        super().__init__(expression_1, expresion_2)
+        
+        
+#------------------------------------------------------------Math-Operations-----------------------------------------------------------------------------------#
+class MathOperationCallNode(Node):
+    def __init__(self, operator, expression) -> None:
         super().__init__()
         self.expression = expression
+        self.operator = operator
+        
+class RandomCallNode(Node):
+    def __init__(self) -> None:
+        super().__init__()
+        
+class LogCallNode(Node):
+    def __init__(self, base, expression) -> None:
+        super().__init__()
+        self.base = base
+        self.expression = expression
+
+#-----------------------------------Let-In--------------------------------------------------------------------------------------------------------------------#
+class LetInNode(Node):
+    def __init__(self, assigments, body) -> None:
+        super().__init__()
+        self.assigments = assigments
+        self.body = body
+
+#----------------------------------Factor-Nodes----------------------------------------------------------------------------------------------------------------#
+class FunctioncallNode(Node):
+    def __init__(self) -> None:
+        super().__init__()
+
+class BooleanNode(Node):
+    def __init__(self, value) -> None:
+        super().__init__()
+        self.value = value
+
+class NumberNode(Node):
+    def __init__(self, value) -> None:
+        super().__init__()
+        self.value = value
+        
+class StringNode(Node):
+    def __init__(self, value) -> None:
+        super().__init__()
+        self.value = value
+        
+class IdentifierNode(Node):
+    def __init__(self) -> None:
+        super().__init__()
+        
+        
         
 
         
 
         
+
