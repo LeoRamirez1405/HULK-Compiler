72d7bcc8c474f616ecdaae35bfefd49681c41b1f
LeoRamirez1405 <leojavramirez1405@gmail.com>
2024-03-27 10:49:01 -0400

    grammar ok

--- grammLR1.py
+++ grammLR1.py
@@ -15,10 +15,11 @@ def gramm_Hulk_LR1():
     identifier, number, string, Elif, Type, Inherits, New, In, def_Type   = G.Terminals('identifier number string elif type inherits new in def_Type') 
     sComil, dComill = G.Terminals('\' \"')
 
+    # Program %= expression
     Program %= statement_list
     statement_list %= statement + statement_list
-    statement_list %= statement
     statement_list %= G.Epsilon
+    # statement_list %= statement
     
     statement %= non_create_statement  
     statement %= create_statement 
@@ -76,31 +77,6 @@ def gramm_Hulk_LR1():
     compAritCond %= NotEqual 
     
     expression_1, expression_2, expression_3, expression_4, expression_5 = G.NonTerminals('expression_1 expression_2 expression_3 expression_4 expression_5')
-    #expression %= Not + expression_1 
-    #expression %= expression_1 
-    #expression_1 %= expression_2 + Is + def_Type 
-    #expression_1 %= expression_2 
-    #expression_2 %= expression_3 + compBoolCond + expression_3
-    #expression_2 %= expression_3
-    #expression_3 %= expression_4 + compAritCond + expression_4 
-    #expression_3 %= _False 
-    #expression_3 %= _True 
-    #expression_3 %= expression_4  
-#
-    #expression_4 %= term + op_term + term 
-    #expression_4 %= term
-    # 
-    
-    #expression %= Not + expression_1
-    #expression_1 %= expression_2 + Is + def_Type 
-    #expression_2 %= expression_3 + compBoolCond + expression_3
-    #expression_4 %= expression_5 + compAritCond + expression_5 
-    #expression_5 %= _False 
-    #expression_5 %= _True 
-#
-    #expression_5 %= term + op_term + term 
-    #expression_5 %= term
-    
     
     expression %= expression_1 + Is + def_Type 
     expression %= expression_1 
@@ -111,19 +87,18 @@ def gramm_Hulk_LR1():
     expression_3 %= Not + expression_4
     expression_3 %= expression_4
     
-    expression_4 %= term + op_term + term 
-    expression_4 %= term
+    expression_4 %= term + op_term
+    op_term %= Plus + term + op_term
+    op_term %= Minus + term + op_term
+    op_term %= G.Epsilon
+    Plus
+    
+    term %= factor + op_factor
+    op_factor %= Mult + factor + op_factor
+    op_factor %= Div + factor + op_factor
+    op_factor %= Mod + factor + op_factor
+    op_factor %= G.Epsilon
 
-    op_term %= Plus 
-    op_term %= Minus
-    
-    term %= factor + op_factor + factor 
-    term %= factor
-    
-    op_factor %= Mult 
-    op_factor %= Div 
-    op_factor %= Mod
-    
     factor %= number
     factor %= oPar + expression + cPar 
     factor %= function_call  
@@ -146,7 +121,7 @@ def gramm_Hulk_LR1():
     # Estructuras adicionales para tipos
     type_definition %= Type + identifier + inheritance + oBrace + attribute_definition + method_definition + cBrace 
     
-    attribute_definition %= kern_assignment + Semi + attribute_definition 
+    attribute_definition %= attribute_definition + kern_assignment + Semi 
     attribute_definition %= G.Epsilon
     
     method_definition %= identifier + oPar + parameters + cPar + oBrace + statement_list + cBrace + method_definition 
@@ -163,5 +138,5 @@ def gramm_Hulk_LR1():
     cont_member %= G.Epsilon   
     
     member %= identifier + cont_member
-    member_access %= expression + Dot + member
+    member_access %= factor + Dot + member
     return G
