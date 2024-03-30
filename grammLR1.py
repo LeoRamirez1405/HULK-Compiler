223f1b125908fd21d29cf0d591e698696b5275ad
LeoRamirez1405 <leojavramirez1405@gmail.com>
2024-03-30 02:35:02 -0400

    GRAMATICA PINCHANDO 2.0 VOLVER EN CASO DE EMERGENCIA

--- grammLR1.py
+++ grammLR1.py
@@ -25,16 +25,15 @@ def gramm_Hulk_LR1():
     statement_list %= oBrace + statement_list + cBrace , lambda h, s: s[2]
     
     statement %= non_create_statement, lambda h, s: s[1] 
-    #statement %= create_statement, lambda h, s: s[1]
+    statement %= create_statement, lambda h, s: s[1]
      
-    #non_create_statement %= control_structure, lambda h, s: s[1]
+    non_create_statement %= control_structure, lambda h, s: s[1]
     non_create_statement %= expr_statement+ Semi , lambda h, s: s[1]
     
-    #create_statement %= let_in, lambda h, s: s[1]
-    #create_statement %= type_definition, lambda h, s: s[1]
-    #create_statement %= function_definition, lambda h, s: s[1]
+    create_statement %= type_definition, lambda h, s: s[1]
+    create_statement %= function_definition, lambda h, s: s[1]
     #create_statement %= assignment, lambda h, s: s[1]
-    #create_statement %= destructive_assignment, lambda h, s: [1]
+    create_statement %= destructive_assignment+ Semi, lambda h, s: s[1]
     
     expr_statement %= print_statement, lambda h, s: s[1]
     expr_statement %= expression, lambda h, s: s[1]
@@ -42,41 +41,41 @@ def gramm_Hulk_LR1():
     
     #let_in_as_expr %= assignment + In + oPar + expr_statement + cPar, lambda h, s: LetInNode(s[1], s[4])
     #let_in %= let_in_as_expr, lambda h,s: s[1]
-    let_in %= assignment + In + oBrace + statement_list + cBrace, lambda h, s: LetInNode(s[1], s[3])
+    #let_in %= assignment + In + oBrace + statement_list + cBrace, lambda h, s: LetInNode(s[1], s[3])
     
     kern_assignment %= identifier + Equal + kern_instance_creation, lambda h, s: KernAssigmentNode(s[1],s[3])
-    #control_structure %= if_structure , lambda h, s: s[1]
-    #control_structure %= while_structure , lambda h, s: s[1]
-    #control_structure %= for_structure , lambda h, s: s[1]
+    control_structure %= if_structure , lambda h, s: s[1]
+    control_structure %= while_structure , lambda h, s: s[1]
+    control_structure %= for_structure , lambda h, s: s[1]
     
-    #if_structure %= If + oPar + condition + cPar + oBrace + statement_list + cBrace + contElif + contElse , lambda h, s: IfStructureNode(s[3], s[6], s[8], s[9])
-    
-    #contElif %= Elif + oPar + condition + cPar + oBrace + statement_list + cBrace + contElif , lambda h, s: [ElifStructureNode(s[3],s[6])] + s[8]
-    #contElif %= G.Epsilon , lambda h, s: []
-    
-    #contElse %= Else + oBrace + statement_list + cBrace , lambda h, s: ElseStructureNode(s[3])
-    #contElse %= G.Epsilon , lambda h, s:  ElseStructureNode([])
-    
-    #while_structure %= While + oPar + condition + cPar + oBrace + statement_list + cBrace , lambda h, s:  WhileStructureNode(s[3], s[6])
-    #for_structure %= For + oPar + assignment + Semi + condition + Semi + destructive_assignment + cPar + oBrace + statement_list + cBrace , lambda h, s:  ForStructureNode(s[3], s[5], s[7], s[10])
+    if_structure %= If + oPar + condition + cPar + oBrace + statement_list + cBrace + contElif + contElse , lambda h, s: IfStructureNode(s[3], s[6], s[8], s[9])
+
+    contElif %= Elif + oPar + condition + cPar + oBrace + statement_list + cBrace + contElif , lambda h, s: [ElifStructureNode(s[3],s[6])] + s[8]
+    contElif %= G.Epsilon , lambda h, s: []
+
+    contElse %= Else + oBrace + statement_list + cBrace , lambda h, s: ElseStructureNode(s[3])
+    contElse %= G.Epsilon , lambda h, s:  ElseStructureNode([])
+
+    while_structure %= While + oPar + condition + cPar + oBrace + statement_list + cBrace , lambda h, s:  WhileStructureNode(s[3], s[6])
+    for_structure %= For + oPar + assignment + Semi + condition + Semi + destructive_assignment + cPar + oBrace + statement_list + cBrace , lambda h, s:  ForStructureNode(s[3], s[5], s[7], s[10])
     
     assignment %= Let + multi_assignment, lambda h, s: s[2]
     multi_assignment %= kern_assignment + Comma + multi_assignment, lambda h, s: [s[1]] + s[3]
     multi_assignment %= kern_assignment + Semi, lambda h, s: [s[1]]
     kern_assignment %= identifier + Equal + expression, lambda h, s: KernAssigmentNode(s[1],s[3])
     
-    #destructive_assignment %= identifier + Destroy + expression + Comma + destructive_assignment, lambda h, s : [DestroyNode(s[1], s[3])] + s[4]
-    #destructive_assignment %= identifier + Destroy + expression, lambda h, s: [DestroyNode(s[1], s[3])]
-    
-    #function_definition %= Function + identifier + type_annotation + oPar + parameters + cPar + oBrace + statement_list + cBrace, lambda h, s: FunctionDefinitionNode(s[2],s[3],s[5],s[8]) 
-    #function_definition %= Function + identifier + type_annotation + oPar + parameters + cPar + Arrow + non_create_statement + Semi,lambda h, s: FunctionDefinitionNode(s[2],s[3],s[5],s[8])
+    destructive_assignment %= identifier + Destroy + expression + Comma + destructive_assignment, lambda h, s : [DestroyNode(s[1], s[3])] + s[4]
+    destructive_assignment %= identifier + Destroy + expression, lambda h, s: [DestroyNode(s[1], s[3])]
+
+    function_definition %= Function + identifier + type_annotation + oPar + parameters + cPar + oBrace + statement_list + cBrace, lambda h, s: FunctionDefinitionNode(s[2],s[3],s[5],s[8]) 
+    function_definition %= Function + identifier + type_annotation + oPar + parameters + cPar + Arrow + non_create_statement + Semi,lambda h, s: FunctionDefinitionNode(s[2],s[3],s[5],s[8])
     
     parameters %= expression + type_annotation + Comma + parameters, lambda h, s: [{s[1]:s[2]}] + s[4]
     parameters %= expression + type_annotation, lambda h, s: {s[1]:s[2]}
     parameters %= G.Epsilon, lambda h, s:[]
     
-    #type_annotation %= Colon + identifier, lambda h, s: TypeNode(s[2]) 
-    #type_annotation %= G.Epsilon, lambda h, s: TypeNode('object')
+    type_annotation %= Colon + identifier, lambda h, s: TypeNode(s[2]) 
+    type_annotation %= G.Epsilon, lambda h, s: TypeNode('object')
     
     ExprAnd, ExprNeg, ExprIsType, ExprComp, ExprNum, ExprOr= G.NonTerminals('ExprAnd ExprNeg ExprIsType ExprComp ExprNum ExprOr')
     
@@ -109,14 +108,14 @@ def gramm_Hulk_LR1():
     ExprNum %= ExprNum + Minus + term , lambda h, s:  SubsExpressionNode(s[1],s[3])
     
     term %= factorPow , lambda h, s: s[1]
-    term %= term + Mult +factorPow, lambda h, s:  MultExpressionNode(s[1],s[3])
+    term %= term + Mult+ factorPow, lambda h, s:  MultExpressionNode(s[1],s[3])
     term %= term + Div + factorPow, lambda h, s:  DivExpressionNode(s[1],s[3])
     term %= term + Mod + factorPow, lambda h, s:  ModExpressionNode(s[1],s[3])
+    
     
     factorPow %= factor, lambda h, s:s[1]
     factorPow %= factor + Pow + factorPow , lambda h, s:  PowExpressionNode(s[1],s[3])
     # term / .... -> let_in_expr
-    # identifier ( identifier ) -> function_call
     factor %= oPar + expr_statement + cPar , lambda h, s:  s[2]
     factor %= number, lambda h, s:  NumberNode(s[1])
     factor %= string, lambda h, s:  StringNode(s[1])
@@ -124,26 +123,10 @@ def gramm_Hulk_LR1():
     factor %= _True, lambda h, s:  BooleanNode(s[1])
     factor %= identifier + oPar + arguments + cPar, lambda h, s: FunctionCallNode(s[1],s[3])
     factor %= identifier, lambda h, s:  IdentifierNode(s[1])
-    #factor %= assignment + In + expr_statement, lambda h, s: LetInExpressionNode(s[1], s[3])
+    factor %= assignment + In + expr_statement + Semi, lambda h, s: LetInExpressionNode(s[1], s[3])
+    factor %= assignment + In + oBrace + statement_list + cBrace, lambda h, s: LetInNode(s[1], s[3])    
     factor %= math_call, lambda h, s:  s[1]
-    factor %= kern_instance_creation, lambda h, s: s[1]
-    factor %= member_access, lambda h, s:  s[1]
-    
-    #factor %= function_call, lambda h, s:  s[1]
-    #function_call %= identifier + oPar + arguments + cPar, lambda h, s: FunctionCallNode(s[1],s[3])
-    #factor %= let_in_as_expr, lambda h,s: s[1]
-    #let_in_as_expr %= assignment + In + expr_statement, lambda h, s: LetInExpressionNode(s[1], s[3])
-    #factor %= number, lambda h, s:  NumberNode(s[1])
-    #factor %= string, lambda h, s:  StringNode(s[1])
-    #factor %= _False, lambda h, s:  BooleanNode(s[1])
-    #factor %= _True, lambda h, s:  BooleanNode(s[1])
-    #factor %= identifier, lambda h, s:  IdentifierNode(s[1])
-    #factor %= oPar + expr_statement + cPar , lambda h, s:  s[2]
-    #factor %= math_call, lambda h, s:  s[1]
-    #factor %= kern_instance_creation, lambda h, s: s[1]
-    #factor %= member_access, lambda h, s:  s[1]
-    
-    
+    factor %= member_access, lambda h, s:  s[1]    
     
     member_access %= factor + Dot + identifier + oPar + arguments + cPar , lambda h, s: MemberAccesNode(s[1], s[3], s[5]) 
     #member_access %= factor + Dot + identifier , lambda h, s: MemberAccesNode(s[1], s[3], [])  #Todo member access Los parametros son privados de la clase #! NAOMI ARREGLA ESTO EN EL CHECKEO SEMANTICO ❤️
@@ -163,18 +146,16 @@ def gramm_Hulk_LR1():
     arguments %= G.Epsilon, lambda h, s: []
     
     # Estructuras adicionales para tipos
-    #type_definition %= Type + identifier + inheritance + oBrace + attribute_definition + method_definition + cBrace, lambda h, s: TypeDefinitionNode(s[2],s[3], s[5], s[6])
-    
-    #attribute_definition %= attribute_definition + kern_assignment + Semi, lambda h, s: s[1] + [s[2]]
-    #attribute_definition %= G.Epsilon, lambda h, s: []
-    
-    #method_definition %= identifier + oPar + parameters + cPar + oBrace + statement_list + cBrace + method_definition, lambda h, s: [FunctionDefinitionNode(s[1], s[3], s[6])] + s[8]
-    #method_definition %= G.Epsilon , lambda h, s: []
-    
-    #inheritance %= Inherits + identifier, lambda h, s: InheritanceNode(s[2])
-    #inheritance %= G.Epsilon, lambda h, s: InheritanceNode("object")
-    
-    
+    type_definition %= Type + identifier + inheritance + oBrace + attribute_definition + method_definition + cBrace, lambda h, s: TypeDefinitionNode(s[2],s[3], s[5], s[6])
+
+    attribute_definition %= attribute_definition + kern_assignment + Semi, lambda h, s: s[1] + [s[2]]
+    attribute_definition %= G.Epsilon, lambda h, s: []
+
+    method_definition %= identifier + oPar + parameters + cPar + oBrace + statement_list + cBrace + method_definition, lambda h, s: [FunctionDefinitionNode(s[1], s[3], s[6])] + s[8]
+    method_definition %= G.Epsilon , lambda h, s: []
+
+    inheritance %= Inherits + identifier, lambda h, s: InheritanceNode(s[2])
+    inheritance %= G.Epsilon, lambda h, s: InheritanceNode("object")
     
     nonzero_digits = '|'.join(str(n) for n in range(1,10))
     zero_digits = '|'.join(str(n) for n in range(0,10))
