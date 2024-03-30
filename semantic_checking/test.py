ed7b2040b333877fc2186a61f0bbb292af57427f
LeoRamirez1405 <leojavramirez1405@gmail.com>
2024-03-30 14:34:43 -0400

    Type def grammar fix

--- semantic_checking/test.py
+++ semantic_checking/test.py
@@ -14,15 +14,17 @@ gramatica, lexer = gramm_Hulk_LR1()
 with open('./prueba.txt', "r", encoding="utf-8") as archivo:
     content = archivo.read()
     text = content
+
+text = 'let number = 42 in let text = "The meaning of life is" in print(text @ number);'
 print(text)
-#text = 'print(42);'
+#text = 'type call(x){ x = 3; }'
 tokens = lexer(text)
 tokentypes = [token.token_type for token in tokens if token.token_type != 'space']
 parser = LR1Parser(gramatica, False)
 output,operations = parser(tokentypes)
 print(type(operations))
 ast = evaluate_reverse_parse(output, operations, tokens)
-#print(ast)
+print(ast)
 #checker = SemanticCheckingVisitor()
 #errors = checker.semantic_checking(ast)
 # print(checker.scope.define_variable)
