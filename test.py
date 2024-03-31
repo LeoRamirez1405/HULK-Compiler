cc610a7940f7e76a6b747b71268ad05d3f3d47d4
Loitzel <loitzelmorales@gmail.com>
2024-03-30 22:10:24 -0400

    ast formatter

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from semantic_checking.AST import *
+from format_visitor import FormatVisitor
 #from semantic_checking.semantic_checking import SemanticCheckingVisitor
 #from semantic_checking.evaluation import evaluate_reverse_parse
 from grammLR1 import gramm_Hulk_LR1
@@ -6,11 +7,16 @@ from LR1 import LR1Parser,evaluate_reverse_parse
 
 gramatica, lexer = gramm_Hulk_LR1()
 
-# with open('./prueba.txt', "r", encoding="utf-8") as archivo:
-#     content = archivo.read()
-#     text = content
+with open('./prueba.txt', "r", encoding="utf-8") as archivo:
+    content = archivo.read()
 
-text = '''let b = a in { print(a);};'''
+    contenido = content.replace('\n', ' ')
+
+    # Reemplazar tabulaciones por espacios en blanco
+    contenido = contenido.replace('\t', '')
+    contenido = contenido.replace('"', '\"')
+    text = content
+
 print(text)
 tokens = lexer(text)
 tokentypes = [token.token_type for token in tokens if token.token_type != 'space']
@@ -19,7 +25,9 @@ output,operations = parser(tokentypes)
 tokensAST = [token for token in tokens if token.token_type != 'space']
 
 ast = evaluate_reverse_parse(output,operations,tokensAST)
-print(ast)
+formatter = FormatVisitor()
+tree = formatter.visit(ast)
+print(tree)
 
 
 
