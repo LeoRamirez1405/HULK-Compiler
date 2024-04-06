bbe1131ce2a9e71ed18c8a45423eb9aeb58a1a11
Loitzel <loitzelmorales@gmail.com>
2024-04-05 20:19:38 -0400

    fixed empty errors

--- grammLR1.py
+++ grammLR1.py
@@ -41,7 +41,7 @@ def gramm_Hulk_LR1():
     #TODO aqui hay que ver como se maneja la cosa de las listas
     #factor %= assignment + In + expr_statement, lambda h, s: LetInExpressionNode(s[1], s[3])
     expr_statement %= assignment + In + expr_statement, lambda h, s: LetInExpressionNode(s[1], s[3], s[2]) #Ya
-    expr_statement %= expression, lambda h, s: [s[1]]
+    expr_statement %= expression, lambda h, s: s[1]
     
     
     print_statement %= Print + oPar + expression + cPar, lambda h, s: PrintStatmentNode(s[3], s[1]) #Ya
